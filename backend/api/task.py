46aa50a244dc995b63dbc8e6ac48e6ac1702a93f
Konstantin Hudyakov <kostja19994@gmail.com>
2021-11-24 19:53:34 +0300

    Implements endpoints for tasks

---
+++ backend/api/task.py
@@ -0,0 +1,88 @@
+from flask import Blueprint, abort, Response, request
+import json
+from psycopg2.errorcodes import FOREIGN_KEY_VIOLATION
+from psycopg2 import errors
+
+from backend.db import get_cursor
+from backend.util import conflict_response, check_authenticated, created_id_response
+
+
+class Task:
+    def __init__(self, id: int, title: str, creator: str, board_id: int) -> None:
+        self.id = id
+        self.title = title
+        self.creator = creator
+        self.board_id = board_id
+
+
+task_blueprint = Blueprint("task", __name__)
+
+
+@task_blueprint.route("/task", methods=["GET"])
+def get_all():
+    check_authenticated()
+    with get_cursor() as cursor:
+        cursor.execute("select tasks.id, tasks.title, users.login, tasks.board_id from tasks "
+                       "inner join boards on boards.id = tasks.board_id "
+                       "inner join users on users.id = tasks.creator_id")
+        res = cursor.fetchall()
+    if res is not None:
+        json_str = json.dumps([Task(id=row[0], title=row[1], creator=row[2], board_id=row[3]).__dict__ for row in res])
+        return Response(json_str, status=200, mimetype="application/json")
+    else:
+        abort(500)
+
+
+@task_blueprint.route("/task/<id>", methods=["GET"])
+def get(id):
+    check_authenticated()
+    try:
+        id = int(id)
+    except ValueError:
+        abort(400)
+    with get_cursor() as cursor:
+        cursor.execute("select tasks.id, tasks.title, users.login, tasks.board_id from tasks "
+                       "inner join boards on boards.id = tasks.board_id "
+                       "inner join users on users.id = tasks.creator_id "
+                       "where tasks.id=%s", [id])
+        res = cursor.fetchone()
+    if res is not None:
+        json_str = json.dumps(Task(id=id, title=res[1], creator=res[2], board_id=res[3]).__dict__)
+        return Response(json_str, status=200, mimetype="application/json")
+    else:
+        abort(404)
+
+
+@task_blueprint.route("/task", methods=["PUT"])
+def create():
+    check_authenticated()
+    title = request.json["title"]
+    creator_id = request.json["creator_id"]
+    board_id = request.json["board_id"]
+    try:
+        creator_id = int(creator_id)
+        board_id = int(board_id)
+    except ValueError:
+        abort(400)
+    if not (0 < len(title) <= 150):
+        return conflict_response("Title too long or empty")
+    try:
+        with get_cursor() as cursor:
+            cursor.execute("insert into tasks(title, creator_id, board_id) VALUES (%s, %s, %s) returning id",
+                           [title, creator_id, board_id])
+    except errors.lookup(FOREIGN_KEY_VIOLATION):
+        return conflict_response("Unknown creator_id or board_id")
+    id = cursor.fetchone()[0]
+    return created_id_response(id)
+
+
+@task_blueprint.route("/task/<id>", methods=["DELETE"])
+def delete(id):
+    check_authenticated()
+    try:
+        id = int(id)
+    except ValueError:
+        abort(400)
+    with get_cursor() as cursor:
+        cursor.execute("delete from tasks where id=%s", [id])
+    return Response(status=200)
--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 
 from backend.api.board import board_blueprint
+from backend.api.task import task_blueprint
 from backend.api.user import user_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(user_blueprint)
 app.register_blueprint(board_blueprint)
+app.register_blueprint(task_blueprint)
 
 cors = CORS(app)
 
