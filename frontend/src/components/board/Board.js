078773fdaf558f7ddc9b2c7bba4058e3b282ad4a
Konstantin Hudyakov <kostja19994@gmail.com>
2021-11-28 13:36:25 +0300

    [front] Implement adding new task

--- frontend/src/components/board/Board.js
+++ frontend/src/components/board/Board.js
@@ -8,7 +8,7 @@ export class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            taskTitle: "",
+            newTaskTitle: "",
             boards: [],
             tasks: []
         }
@@ -35,6 +35,63 @@ export class Board extends React.Component {
                 }).catch(error => console.log(error))
             }
         }).catch(error => console.log(error))
+    }
+
+    handleTaskAdd() {
+        if (!this.checkTitle()) return
+        const task = {
+            title: this.state.newTaskTitle,
+            creator_id: this.props.userId,
+            board_id: this.state.boards[0].id
+        }
+        this.createTask(task)
+    }
+
+    createTask(task) {
+        const request = new Request(`${SERVER_URL}/task`, {
+            method: "PUT",
+            headers: this.props.getDefaultHeaders(),
+            body: JSON.stringify(task)
+        })
+        fetch(request).then(response => {
+            this.setState({newTaskTitle: ""})
+            if (response.status === 400) {
+                throw createError(request, response, "Bad request")
+            } else if (response.status === 403) {
+                this.props.onLogout()
+            } else if (response.status === 409) {
+                response.text().then(errorText => {
+                    if (errorText === "Title too long or empty") {
+                        alert(errorText)
+                    } else {
+                        throw createError(request, response, errorText)
+                    }
+                })
+            } else if (!response.ok) {
+                throw createError(request, response)
+            } else {
+                response.json().then(data => {
+                    task.id = data.id
+                    task.creator = this.props.userName
+                    const tasks = this.state.tasks
+                    tasks.push(task)
+                    this.setState({tasks: tasks})
+                })
+            }
+        }).catch(error => console.log(error))
+    }
+
+    checkTitle() {
+        const title = this.state.newTaskTitle
+        let everythingOk = true
+        if (title.length === 0) {
+            alert("Please specify your title for new task")
+            everythingOk = false
+        } else if (title.length > 150) {
+            alert("Title is too long")
+            everythingOk = false
+        }
+        return everythingOk
     }
 
     render() {
@@ -64,10 +121,12 @@ export class Board extends React.Component {
             <div className="board-header" id="board-header">
                 <label className="form-label board-header-label">Scrum Board</label>
                 <div className="board-header-add-input">
-                    <input className="form-control add-task" value={this.state.taskTitle} placeholder="Title..."
+                    <input className="form-control add-task" value={this.state.newTaskTitle} placeholder="Title..."
                            type={"title"}
-                           onChange={(event) => this.setState({taskTitle: event.target.value})}/>
-                    <button className={"btn btn-primary add-task"}>Add Task</button>
+                           onChange={(event) => this.setState({newTaskTitle: event.target.value})}/>
+                    <button className={"btn btn-primary add-task"}
+                            onClick={() => this.handleTaskAdd()}>Add Task
+                    </button>
                 </div>
             </div>
         )
